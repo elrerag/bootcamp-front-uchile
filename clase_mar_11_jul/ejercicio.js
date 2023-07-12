3d39ef995a999afc1d69f49ffaa2fd8da464abbb
Luis Herrera <elrerag@gmail.com>
2023-07-11 21:23:05 -0400

    ..

--- clase_mar_11_jul/ejercicio.js
+++ clase_mar_11_jul/ejercicio.js
@@ -1,41 +1,34 @@
-// valida el rango
 function validarRango(numero) {
-  return numero >= 1 && numero <= 1000;
+    return numero >= 1 && numero <= 1000;
 }
 
 function calcularPorcentaje(numero_uno, numero_dos) {
-  return (numero_dos / numero_uno) * 100;
+    return (numero_dos / numero_uno) * 100;
 }
 
 function calcularSuma(numero_uno, numero_dos) {
-  return numero_uno + numero_dos;
+    return numero_uno + numero_dos;
 }
 
 function calcularResta(numero_uno, numero_dos) {
-  return numero_uno - numero_dos;
+    return numero_uno - numero_dos;
 }
 
-function main() {
-  const numero_uno = 11;
-  const numero_dos = 10;
+const numero_uno = 11;
+const numero_dos = 10;
 
-  if(validarRango(numero_uno) && validarRango(numero_dos)){
+if (validarRango(numero_uno) && validarRango(numero_dos)) {
     if (numero_uno > numero_dos) {
         const porcentaje = calcularPorcentaje(numero_uno, numero_dos);
         const suma = calcularSuma(numero_uno, numero_dos);
         const resta = calcularResta(numero_uno, numero_dos);
-    
+
         console.log(`Porcentaje: ${porcentaje}%`);
         console.log(`Suma: ${suma}`);
         console.log(`Resta: ${resta}`);
-      } else {
+    } else {
         console.log('El numero_uno debe ser mayor que el numero_dos');
-      }
-  }else{
+    }
+} else {
     console.log('Las variables numero_uno y numero_dos solo pueden ser números de 1 a 1000');
-  }
-
-
 }
-
-main();
